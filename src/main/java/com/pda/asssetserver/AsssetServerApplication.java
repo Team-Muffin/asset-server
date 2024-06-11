860142f5da333b0647048586583d7faf8378f823
Dongwon KIm <dongwon000103@gmail.com>
2024-06-11 15:32:01 +0900

    init: asset server

---
+++ src/main/java/com/pda/asssetserver/AsssetServerApplication.java
@@ -0,0 +1,13 @@
+package com.pda.asssetserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsssetServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsssetServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pda/asssetserver/AsssetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pda.asssetserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsssetServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
